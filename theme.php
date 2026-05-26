036ef8a8fae42f6a6f838f07b768e6f387430854
Steffen Winde <inserv@winde-ganzig.de>
2026-05-26 10:06:22 +0200

    Füge neue Verzeichnisse für benutzerdefinierte Vorlagen hinzu

---
+++ theme.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+$aTheme = [
+    'id' => 'o3-child',
+    'title' => 'CHILD',
+    'description' => 'A child theme from o3-shop.',
+    'parentTheme' => 'o3-shop',
+    'parentVersions' => array($oTheme->getInfo('version')),
+];
